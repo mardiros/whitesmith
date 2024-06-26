da0fe77c5eeb0a742bdb7bdc1e414b2af112d76d
Guillaume Gauvrit <guillaume@gauvr.it>
2024-06-26 07:56:12 +0200

    Remove pkg_resources

--- src/whitesmith/__init__.py
+++ src/whitesmith/__init__.py
@@ -1,8 +1,8 @@
-import pkg_resources
+from importlib import metadata
+
 
 from .model import HTTPCollectionResponse, HTTPResponse
 from .router import router
 
-__version__ = pkg_resources.get_distribution("whitesmith").version
-
+__version__ = metadata.version("whitesmith")
 __all__ = ["HTTPResponse", "HTTPCollectionResponse", "router", "__version__"]
