21c5991767285a059d6a91761edc4ec9463d1bf0
Guillaume Gauvrit <guillaume.gauvrit@gandi.net>
2025-12-16 14:13:43 +0100

    Add tests for generate-openapi

--- tests/unittests/entrypoints/test_cli.py
+++ tests/unittests/entrypoints/test_cli.py
@@ -7,3 +7,16 @@ def test_generate_fixtures(tmp_path: Path):
     main(["whitesmith", "generate", "-o", str(tmp_path), "-m", "tests.resources"])
     files = {f.name: f.read_text() for f in tmp_path.glob("whitesmith_handlers/*.py")}
     assert files.keys() == {"__init__.py", "address.py", "notif.py", "organization.py"}
+
+
+def test_generate_openapi(tmp_path: Path):
+    main(
+        ["whitesmith", "generate-openapi", "-o", str(tmp_path), "-m", "tests.resources"]
+    )
+    files = {f.name: f.read_text() for f in tmp_path.glob("*")}
+    assert files.keys() == {
+        "index.html",
+        "address.json",
+        "notif.json",
+        "organization.json",
+    }
